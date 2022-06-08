e39c9ca7459f3d2f1c2c138873dde3cf247ef08c
Sven Hilsmann <sven.hilsmann.ext@uniper.energy>
2022-06-08 10:34:58 +0200

    Change URLs to new Repo

--- app/layout/footer.php
+++ app/layout/footer.php
@@ -10,8 +10,8 @@ define('FOOTER_TEMPLATE', 1);
             <div class="btn-group large">
                 <a href="?page=installation" class="btn btn-default" data-toggle="tooltip" title="Reset DIWA's Database"><i class="fa fa-bomb" aria-hidden="true"></i></a>
                 <a href="https://choosealicense.com/licenses/mit/" target="_blank" class="btn btn-default" data-toggle="tooltip" title="This Project is licensed under MIT License"><i class="fa fa-gavel" aria-hidden="true"></i></a>
-                <a href="https://github.com/snsttr/diwa" target="_blank" class="btn btn-default" data-toggle="tooltip" title="DIWA's GitHub Repository"><i class="fa fa-github" aria-hidden="true"></i></a>
-                <a href="https://github.com/snsttr/diwa/issues" target="_blank" class="btn btn-default" data-toggle="tooltip" title="Report a Bug or suggest an Improvement"><i class="fa fa-bug" aria-hidden="true"></i></a>
+                <a href="https://github.com/hilsmann/diwa" target="_blank" class="btn btn-default" data-toggle="tooltip" title="DIWA's GitHub Repository"><i class="fa fa-github" aria-hidden="true"></i></a>
+                <a href="https://github.com/hilsmann/diwa/issues" target="_blank" class="btn btn-default" data-toggle="tooltip" title="Report a Bug or suggest an Improvement"><i class="fa fa-bug" aria-hidden="true"></i></a>
             </div>
         </footer>
     </div>
